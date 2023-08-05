a0040e348faf0cfa462b793a6fdb53724924c6f4
Your Name <you@example.com>
2023-08-05 20:14:55 +0200

    movie rated scrapping

--- movie_scrap.py
+++ movie_scrap.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
@@ -9,8 +10,18 @@ response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, 'html.parser')
 #print(soup.prettify())
 movies=soup.find_all('h3',{'class':'ipc-title__text'})
-print(movies)
-for movie in movies:
-        title=movie.text
-        print(f"title is {title}")
-        #print(title)
+rating=soup.find_all('span',{'class':'ipc-rating-star'})
+#print(rating)
+#print(movies)
+i=0
+with open('Movies.csv','w',newline='',encoding='utf-8') as file:
+     writer=csv.writer(file)
+     writer.writerow(['Movie Title ','Movie Rating'])
+     for movie, rate in zip(movies, rating):
+        title = movie.text
+        movie_rating = rate.text
+        i+=1
+        if i<=251:
+          print(f"{title}  - {movie_rating}")
+          writer.writerow([title,movie_rating])
+print("Done!")
